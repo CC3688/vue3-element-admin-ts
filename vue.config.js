72c78967b5f896ad5f156ec0b29220abaf6dd70d
chenjunwu <442677028@qq.com>
2021-12-02 15:09:40 +0800

    element-plus

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,14 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
+
 module.exports = {
   lintOnSave: false,
+  configureWebpack: {
+    plugins: [
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+  },
 }
