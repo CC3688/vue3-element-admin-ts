6010d5ef29e8da6e4966f1295542e2d042c06d4c
chenjunwu <442677028@qq.com>
2021-12-28 14:15:14 +0800

    add auto import

--- src/views/deal/general.vue
+++ src/views/deal/general.vue
@@ -1,10 +1,15 @@
+<script lang="ts">
+export default { name: 'DealGeneral' }
+</script>
 <script setup lang="ts">
+// eslint-disable-next-line import/first
 import { ref } from 'vue'
 
 const text = ref('')
 
 console.log('DealGeneral ....')
 </script>
+
 <template>
   <div>
     <div><input type="text" v-model="text" /></div>
