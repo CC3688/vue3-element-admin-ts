51f9d6a4a8ee29b9038d36bda6ea4818be8f2ffd
chenjunwu <442677028@qq.com>
2021-12-29 14:27:57 +0800

    bundle

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+const AutoImport = require('unplugin-auto-import/webpack')
 const Components = require('unplugin-vue-components/webpack')
 const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 
@@ -8,16 +9,24 @@ function resolve(dir) {
   return path.join(__dirname, dir)
 }
 
+console.log('process.env.NODE_ENV::', process.env.NODE_ENV)
+console.log('process.env.VUE_APP_ENV::', process.env.VUE_APP_ENV)
+
 module.exports = {
   lintOnSave: false,
   configureWebpack: {
     plugins: [
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
       Components({
         resolvers: [ElementPlusResolver()],
       }),
     ],
   },
   chainWebpack(config) {
+    config.output.filename('js/[name].[hash].js').end()
+    config.output.chunkFilename('js/[name].[hash].js').end()
     config.module.rule('svg').exclude.add(resolve('src/icons')).end()
     config.module
       .rule('icons')
@@ -30,5 +39,54 @@ module.exports = {
         symbolId: 'icon-[name]',
       })
       .end()
+
+    config.when(process.env.VUE_APP_ENV !== 'development', (config) => {
+      config
+        .plugin('ScriptExtHtmlWebpackPlugin')
+        .after('html')
+        .use('script-ext-html-webpack-plugin', [{ inline: /runtime\..*\.js$/ }])
+        .end()
+
+      config.optimization.splitChunks({
+        chunks: 'all',
+        cacheGroups: {
+          libs: {
+            name: 'chunk-libs',
+            test: /[\\/]node_modules[\\/]/,
+            priority: 10,
+            chunks: 'initial', // only package third parties that are initially dependent
+          },
+          elementIcons: {
+            name: 'chunk-element-icons', // split elementUI into a single package
+            priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+            test: /[\\/]node_modules[\\/]_?@element-plus(.*)/, // in order to adapt to cnpm
+          },
+          elementUI: {
+            name: 'chunk-element-plus', // split elementUI into a single package
+            priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+            test: /[\\/]node_modules[\\/]_?element-plus(.*)/, // in order to adapt to cnpm
+          },
+          echarts: {
+            name: 'chunk-echarts', // split elementUI into a single package
+            priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+            test: /[\\/]node_modules[\\/]_?echarts(.*)/, // in order to adapt to cnpm
+          },
+          commons: {
+            name: 'chunk-commons',
+            test: resolve('src/components'), // can customize your rules
+            minChunks: 3, //  minimum common number
+            priority: 5,
+            reuseExistingChunk: true,
+          },
+          utils: {
+            name: 'chunk-utils',
+            test: resolve('src/utils'), // can customize your rules
+            minChunks: 3, //  minimum common number
+            priority: 5,
+            reuseExistingChunk: true,
+          },
+        },
+      })
+    })
   },
 }
